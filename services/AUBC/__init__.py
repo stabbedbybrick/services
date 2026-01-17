9b070714c5b751dccb238badfaa00516b64d80f7
stabbedbybrick <stabbedbybrick@proton.me>
2026-01-17 12:00:38 +0100

    AUBC -> v1.0.4
    - Improve the addition of 1080p streams

--- services/AUBC/__init__.py
+++ services/AUBC/__init__.py
@@ -24,7 +24,7 @@ class AUBC(Service):
     Service code for ABC iView streaming service (https://iview.abc.net.au/).
 
     \b
-    Version: 1.0.3
+    Version: 1.0.4
     Author: stabbedbybrick
     Authorization: None
     Robustness:
@@ -119,8 +119,8 @@ class AUBC(Service):
         captions = next((x.get("captions") for x in playlist if x["type"] == "program"), None)
         title.data["protected"] = streams.get("protected", False)
 
-        if "720" in streams:
-            streams["1080"] = streams["720"].replace("720", "1080")
+        if hd := streams.get("720"):
+            streams["1080"] = hd.replace("720.mpd", "1080.mpd")
 
         manifest = next(
             (url for key in ["1080", "720", "sd", "sd-low"] if key in streams
@@ -130,7 +130,7 @@ class AUBC(Service):
         )
         if not manifest:
             raise ValueError("Could not find a manifest for this title")
-
+        
         tracks = DASH.from_url(manifest, self.session).to_tracks(title.language)
 
         for track in tracks.audio:
